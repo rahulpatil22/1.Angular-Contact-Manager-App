c44eb6977146d14fc828c1068be1cfb05c6708eb
Rahul Gopal Patil <rahulil@cybage.com>
2023-09-12 18:40:40 +0530

    done

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    ViewContactComponent,
+    SpinnerComponent,
+    EditContactComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/IContact.ts
@@ -0,0 +1,10 @@
+export interface IContact {
+    id? : string;
+    name: string;
+    email:string;
+    photo:string;
+    mobile:string;
+    compnay:string;
+    title:string;
+    groupId:string;
+}
